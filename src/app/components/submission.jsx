d24a8db629e6f56ca0755d4c628222ac55e6778b
24LP7757 24LP7757 <24LP7757@G302-CC0120.local>
2024-12-17 19:52:55 +0800

    Form day1

---
+++ src/app/components/submission.jsx
@@ -0,0 +1,10 @@
+export default function Submission(){
+    return(
+        <div className="w-[32rem] h-[10rem] bg-white rounded-[8px] ml-auto mr-auto ">
+        <img src="Main.png" className="pt-6 pl-4 w-[60px] h-[66px] ml-6" /> 
+        <h1 className="font-bold text-[2rem] pl-10">You're All Set 🔥</h1>
+        <p className="pl-10 text-[#8E8E8E]">We have received your submission. Thank you!</p>
+        </div>
+        
+    );
+}
